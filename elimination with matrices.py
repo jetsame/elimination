c21c004fd038c268194f32d7a950dd671877988c
jetsame <43726901+jetsame@users.noreply.github.com>
2018-09-30 20:14:14 +0300

    Update elimination with matrices.py

--- elimination with matrices.py
+++ elimination with matrices.py
@@ -1,33 +1,37 @@
-import numpy as np #import moudule for  work with numpy (matrices)
+"""
+//// Rustam Silin//////
+"""
+import numpy as np 
 
-def elimination(a, b, c, d, e, f, h, g, k): #create function which take elements of matrix
-    matrix = np.zeros((0,3)) #create matrix with all rows = 0 and 3 colunns
-    matrix = np.concatenate((matrix, [[a, b, c], [d, e, f], [h, g, k]])) #add our arguments as elements of matrix
+def elimination(a, b, c, d, e, f, h, g, k): #elements of matrix (3x3)
+    matrix = np.zeros((0,3))
+    matrix = np.concatenate((matrix, [[a, b, c], [d, e, f], [h, g, k]])) 
     
-    if a == 0: #do condition
-        matrix = np.concatenate((matrix, [[d, e, f], [a, b, c], [h, g, k]])) #switch rows when the first element of matrix is equal to 0
-        print(d) #print d as first element of elimination
+    if a == 0: #do row exchange
+        matrix = np.concatenate((matrix, [[d, e, f], [a, b, c], [h, g, k]])) 
+        print(d)
         if d == 0:
-            matrix = np.concatenate((matrix, [[h, g, k], [d, e, f], [a, b, c]]))
-            if h == 0:
+            matrix = np.concatenate((matrix, [[h, g, k], [d, e, f], [a, b, c]])) #do again row exchange
+            if h == 0: #if have all zeroes in first column
                 print('Matrix is Singular.', 'Look at row 1-column 1, row 2-column 1 and row 3- column 1')
                 print('Right now will be an error. Dont worry.')
     else: 
-        if a < d and a < h: #for d and h in elimination
-           result_of_a = -(d // a) #find the pivot for 'a' which converts the first column (besides first element of first column)
-           result_2_of_a = -(h // a) #find the pivot for 'a' which converts the first column (besides first element of first column)
+        if a < d and a < h: 
+           result_of_a = -(d // a) #find the multiplier for 2,1 position
+           result_2_of_a = -(h // a) #for 3,1 position
            
-           zero_d = result_of_a + a  
-           new_e = result_of_a + b #add the result to find the second row of the matrix
-           new_f = result_of_a + c
+           zero_d = result_of_a + a #new 2,1 position (zero) 
+           new_e = result_of_a + b #2,2
+           new_f = result_of_a + c #2,3
            
-           zero_h = result_2_of_a + h #create 0 in position letter 'h'
+           zero_h = result_2_of_a + h #zero (3,1 position) 
            
-           matrix = np.concatenate((matrix, [[a, b, c], [zero_d, new_e, new_f], [zero_h, g, k]])) #write the new matrix 
-           print(a)
-    if  new_e == 0: #do condition
-        matrix = np.concatenate((matrix, [[zero_d, new_e, new_f], [a, b, c], [zero_h, g, k]])) #switch rows when the second element of matrix is equal to 0
-        print(h) #print new as the second element of elemination
+           matrix = np.concatenate((matrix, [[a, b, c], [zero_d, new_e, new_f], [zero_h, g, k]])) #convert the matrix
+           print(a) #print 1 pivot
+    
+    if  new_e == 0: 
+        matrix = np.concatenate((matrix, [[zero_d, new_e, new_f], [a, b, c], [zero_h, g, k]])) 
+        print(h)
         if b == 0:
             print('Matrix is Singular.', 'Look at row 1-column 2, row 2-column 2 and row 3- column 2')
     else:
@@ -37,6 +41,6 @@ def elimination(a, b, c, d, e, f, h, g, k): #create function which take elements
             new_g = result_of_new_e + new_e
             new_k = result_of_new_e + new_f
             
-            matrix = np.concatenate((matrix, [[a, b, c], [zero_d, new_e, new_f], [zero_h, new_g, new_k]]))
+            matrix = np.concatenate((matrix, [[a, b, c], [zero_d, new_e, new_f], [zero_h, new_g, new_k]])) #convert to the final matrix
             print('', new_e, ' \n', new_k)
-elimination(0, 3, 0, 0, 7, 0, 0, 7, 1)
+elimination(0, 3, 0, 0, 7, 0, 0, 7, 1) #for example (output error)
